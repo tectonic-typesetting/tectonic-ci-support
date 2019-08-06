3087e691f9e9a8e9cd979764ea07bab6bb25c704
Peter Williams <peter@newton.cx>
2019-08-06 09:39:02 -0400

    bigendian/make-chroot.sh: make the output filename agree with what the main Tectonic CI uses

--- bigendian/make-chroot.sh
+++ bigendian/make-chroot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-# Copyright 2018 the Tectonic Project
+# Copyright 2018-2019 the Tectonic Project
 # Licensed under the MIT License.
 #
 # All-in-one script to make the chroot tarball.
 
 set -e -x
 vagrant up
-vagrant ssh -c "sudo tar czf /vagrant/buildenv-powerpc.tar.gz -C /buildenv-powerpc bin dev etc lib root run sbin usr var"
+vagrant ssh -c "sudo tar czf /vagrant/tectonic-buildenv-ppc.tar.gz -C /buildenv-powerpc bin dev etc lib root run sbin usr var"
