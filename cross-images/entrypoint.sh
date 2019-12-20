c1ab99f9c44d3beca3f596f7701ef6641287f1b2
Peter Williams <peter@newton.cx>
2019-12-20 17:34:30 -0500

    cross-images: UID handling wasn't fully baked -- needs to happen inside alpine too

--- cross-images/entrypoint.sh
+++ cross-images/entrypoint.sh
@@ -20,23 +20,28 @@ fi
 
 # Now, ensure that there exist a user and group with the desired UID/GID,
 # since the container may have been built with settings different than
-# expected on the host system.
+# expected on the host system. We also have to do this inside the alpine
+# chroot since we need to "su" back to the user within the chroot execution.
 
 groupname="$(getent group $HOST_GID |cut -d: -f1 || true)"
 if [ -z "$groupname" ] ; then
     groupname="gid${HOST_GID}"
     addgroup -q --gid $HOST_GID "$groupname"
+    /alpine/enter-chroot addgroup -g $HOST_GID "$groupname"
 fi
 
 username="$(getent passwd $HOST_UID |cut -d: -f1 || true)"
 if [ -z "$username" ] ; then
     username="user${HOST_UID}"
     adduser -q --gid $HOST_GID --uid $HOST_UID --disabled-password --gecos "UID $HOST_UID" "$username"
+    /alpine/enter-chroot adduser -D -G "$groupname" -u $HOST_UID "$username"
 fi
 
 cur_prim_gid="$(getent passwd $HOST_UID |cut -d: -f4)"
 if [ "$cur_prim_gid" -ne "$HOST_GID" ] ; then
     usermod -g $HOST_GID "$username"
+    /alpine/enter-chroot deluser "$username"
+    /alpine/enter-chroot adduser -D -G "$groupname" -u $HOST_UID "$username"
 fi
 
 # We can now run the command with the desired UID/GID.
