ca8e8f25141abca13a3cda05ca90c1cf993dd74a
Peter Williams <peter@newton.cx>
2022-06-23 13:41:50 -0400

    bigendian: remove this

--- bigendian/make-chroot.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Copyright 2018-2019 the Tectonic Project
-# Licensed under the MIT License.
-#
-# All-in-one script to make the chroot tarball.
-
-set -e -x
-vagrant up
-vagrant ssh -c "sudo tar czf /vagrant/tectonic-buildenv-ppc.tar.gz -C /buildenv-powerpc bin dev etc lib root run sbin usr var"
